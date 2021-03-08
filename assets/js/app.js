bef7302714848372f47b5577580344bc29e6a451
ProfeMoy <78748094+ProfeMoy@users.noreply.github.com>
2021-03-08 11:56:03 -0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    prueba1();
+});
+/*
+function test() {
+    console.log("Ola k ase?")
+}*/
+
+$("#verde").click(function() {
+    $("#color").removeClass().addClass("bg-success");
+    $("#texto").text("Verde");
+    console.log("Verde");
+});
+
+$("#amarillo").click(function() {
+    $("#color").removeClass().addClass("bg-warning");
+    $("#texto").text("Amarillo");
+    console.log("Amarillo");
+});
+
+$("#rojo").click(function() {
+    $("#color").removeClass().addClass("bg-danger");
+    $("#texto").text("Rojo");
+    console.log("Rojo");
+});
+
+$("#imagen").click(function() {
+    $("#color").removeClass().addClass("bgIMG");
+    $("#texto").text("Imagen");
+    console.log("Imagen");
+});
+
+
+
+
+function prueba1() {
+    var x = "Ola k ase?";
+    console.log(x);
+    prueba2(x);
+}
+
+function prueba2(pepe) {
+    console.log(pepe);
+}
